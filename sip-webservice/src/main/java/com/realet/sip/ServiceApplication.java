9506ada56aca5f204859054f813ffd38664bd4f7
realet1337 <lol.12345689028@gmail.com>
2021-03-13 00:56:37 +0100

    Changed context root

--- sip-webservice/src/main/java/com/realet/sip/ServiceApplication.java
+++ sip-webservice/src/main/java/com/realet/sip/ServiceApplication.java
@@ -4,11 +4,14 @@ import java.util.HashSet;
 import java.util.Set;
 
 import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.GET;
+import javax.ws.rs.Produces;
 import javax.ws.rs.core.Application;
+import javax.ws.rs.core.MediaType;
 
 import com.realet.sip.UsersResource;
 
-@ApplicationPath("/api")
+@ApplicationPath("/")
 public class ServiceApplication extends Application {
     
     @Override
@@ -16,6 +19,12 @@ public class ServiceApplication extends Application {
         Set<Class<?>> set = new HashSet<>();
         set.add( UsersResource.class );
         return set;
+    }
+
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    public String getUsers() {
+        return "Main ";
     }
 
 }

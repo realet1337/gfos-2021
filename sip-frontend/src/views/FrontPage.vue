c8eb0d5a2acc87aa4bbcd03c296007f75d0e157e
realet1337 <lol.123456789028@gmail.com>
2021-04-23 21:06:04 +0200

    Front Page done

--- sip-frontend/src/views/FrontPage.vue
+++ sip-frontend/src/views/FrontPage.vue
@@ -1,8 +1,32 @@
 <template>
-    <a href="/login">login</a>
+    <v-app>
+        <v-main>
+            <canvas class="background-particles"></canvas>
+            <v-container fluid fill-height class="black">
+                <v-row justify="center">
+                    <v-col align-self="center">
+                        <v-img @click="$router.push('login')" class="mx-auto clickable" width="400" src="@/assets/sip.png"></v-img>
+                    </v-col>
+                </v-row>
+            </v-container>
+        </v-main>
+    </v-app>
 </template>
 <script>
 export default {
-    name: "FrontPage.vue",
+    name: "FrontPage",
+    created: function(){
+        this.$nextTick(
+            function(){
+                window.Particles.init({
+                    selector: '.background-particles',
+                    connectParticles: true,
+                    color: this.$vuetify.theme.currentTheme.primary,
+                    sizeVariations: 1,
+                    maxParticles: 100,
+                });
+            }
+        );
+    }
 }
 </script>

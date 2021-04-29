690bf16c8cf1e7337d341e433b5c041f6f6d1077
realet1337 <lol.123456789028@gmail.com>
2021-04-30 01:54:17 +0200

    Bug fix

--- sip-webservice/src/main/java/com/realet/sip/ImagesResource.java
+++ sip-webservice/src/main/java/com/realet/sip/ImagesResource.java
@@ -163,6 +163,10 @@ public class ImagesResource {
         if(image == null){
             return Response.status(400).build();
         }
+
+        BufferedImage newBufferedImage = new BufferedImage(image.getWidth(),
+        image.getHeight(), BufferedImage.TYPE_INT_RGB);
+        newBufferedImage.getGraphics().drawImage(image, 0, 0, null);
         
         String outName = "";
         File outputFile;
@@ -184,7 +188,6 @@ public class ImagesResource {
                 path = path + "/pic/group/" + outName + ".jpg";
 
                 outputFile = new File(path);
-                outputFile.getParentFile().mkdirs();
 
             }while(outputFile.exists()); //Just keep going until we get one.
 
@@ -193,9 +196,10 @@ public class ImagesResource {
             return Response.status(500).build();
         }
 
+        outputFile.getParentFile().mkdirs();
 
         try (OutputStream os = new FileOutputStream(outputFile)) {
-            ImageIO.write(image, "jpg", os);
+            ImageIO.write(newBufferedImage, "jpg", os);
         } catch (IOException e) {
             return Response.status(500).entity("Couldn't create image.").build();
         }
